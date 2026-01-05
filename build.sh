1249b55936ecf7ae94024c85b6fa040f22ba53fa
Yannick Plassiard <yan@mtyp.fr>
2026-01-05 12:15:37 +0100

    Disable crashpad on macOS for SDK compatibility

--- build.sh
+++ build.sh
@@ -261,6 +261,7 @@ angle_enable_swiftshader=false
 enable_swiftshader_vulkan=false
 angle_enable_vulkan=false
 use_dawn=false
+use_crashpad=false
 "
     elif [ "$OS" = "Linux" ]; then
         GN_ARGS+="

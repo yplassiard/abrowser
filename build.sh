54d70d4265a0fa6b6b339afaba473deed52cb078
Yannick Plassiard <yan@mtyp.fr>
2026-01-05 11:34:20 +0100

    Add libx11-dev and libxcb1-dev for X11 support

--- build.sh
+++ build.sh
@@ -67,7 +67,8 @@ install_dependencies() {
                 libsecret-1-dev \
                 libxss-dev libxtst-dev \
                 libpulse-dev libudev-dev \
-                libva-dev libcurl4-openssl-dev
+                libva-dev libcurl4-openssl-dev \
+                libx11-dev libxcb1-dev
         else
             echo "Unsupported Linux distribution. Please install dependencies manually."
             exit 1
